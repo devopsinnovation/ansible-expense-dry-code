91fc99042414ccf2a7e0bac576648c930b60a4cf
aditya <adityavittal92@gmail.com>
2024-05-18 19:12:48 +0530

    updated the url for the frontend bavkend and the mysql

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 ansible-playbook -i ${component}-${env}.devopsinnovation.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD}
--e role_name=${component} expense.yml
+-e role_name=${component} -e env=dev expense.yml
